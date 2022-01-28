8daf02bae1742458b04f63f68dd9d085af4cce27
Simon Huang <huisimonliu@gmail.com>
2022-01-28 01:02:31 -0800

    First commit for unity FlappyBirdGame

---
+++ Assets/Code/ObstacleMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D rb2d;
+    GameObject obstacle;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        obstacle = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.velocity = new Vector2(-1.0f, 0.0f);
+
+        if(rb2d.position.x  < -1.0f)
+        {
+        	Debug.Log("Out Of Camera Left");
+        	Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Code/player_controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+	Rigidbody2D rb2d;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+        	anim.SetBool("isClicked", true);
+        	rb2d.velocity = new Vector2(0.0f, 0.0f);
+        	Debug.Log(rb2d.velocity);
+            rb2d.AddForce(transform.up * 1f, ForceMode2D.Impulse );
+
+        }
+        else
+        {
+        	if(rb2d.velocity.y < 0)
+        		anim.SetBool("isClicked", false);
+        }
+
+        //if()
+        	Debug.Log("Collision");
+    }
+}
